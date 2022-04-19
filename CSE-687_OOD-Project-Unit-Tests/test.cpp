f2539555d4d361dc71e38d28d8e3f56e09b8c6fe
Mclemore, Ciante (US) <ciante.mclemore@lmco.com>
2022-04-19 15:26:57 -0400

    Updated the "test".cpp so that we can split our unit test out by file instead of nesting them all into this single file. Now "test.cpp" will run all the test in the project.

--- CSE-687_OOD-Project-Unit-Tests/test.cpp
+++ CSE-687_OOD-Project-Unit-Tests/test.cpp
@@ -1,36 +1,6 @@
 #include "pch.h"
-#include "regex"
-#include "../CSE-687_OOD-Project/Utility.h"
 
-class FileUtility : public ::testing::Test {
-protected:
-	void SetUp() override {
-		input1 = "";
-		input2 = "The brown fox jumped the fence";
-		input3 = "There's";
-		input4 = "The!re";
-		input5 = "T.here";
-	}
-public:
-	std::string input1;
-	std::string input2;
-	std::string input3;
-	std::string input4;
-	std::string input5;
-};
-
-TEST_F(FileUtility, FileUtilityTestSplitAndClean) {
-	auto x = Utility::SplitAndClean(input1);
-	EXPECT_EQ(x, std::list<std::string>());
-	auto y = Utility::SplitAndClean(input2);
-	EXPECT_EQ(y.size(), 6);
-}
-
-TEST_F(FileUtility, FileUtilityTestRemoveSpecialCharacters) {
-	auto x = Utility::RemoveSpecialCharacters(input3);
-	auto y = Utility::RemoveSpecialCharacters(input4);
-	auto z = Utility::RemoveSpecialCharacters(input5);
-	EXPECT_FALSE(std::regex_search(x, std::regex("[^a-zA-Z0-9]+"))) << "The input contains special characters." << std::endl;
-	EXPECT_FALSE(std::regex_search(y, std::regex("[^a-zA-Z0-9]+"))) << "The input contains special characters." << std::endl;
-	EXPECT_FALSE(std::regex_search(z, std::regex("[^a-zA-Z0-9]+"))) << "The input contains special characters." << std::endl;
+int main(int argc, char* argv[]) {
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
 }
