89cbfd63174f654bb2a75b9b39e65ddd45778d06
Clayton Colson <cecolson1997@gmail.com>
2022-04-26 22:26:15 -0400

    I was able to get this to run, but it failed... not sure what the issue is.

--- CSE-687_OOD-Project-Unit-Tests/ReducerTest.cpp
+++ CSE-687_OOD-Project-Unit-Tests/ReducerTest.cpp
@@ -4,14 +4,16 @@
 class ReduceAndOutputTest : public ::testing::Test {
 public:
 	ReduceAndOutputTest() {
-		testContainer["the"] = { 1, 1, 1 };
-		testContainer["and"] = { 1, 1 };
-		testContainer["who"] = { 1 };
-		testContainer["what"] = { 1, 1 };
-		testContainer["when"] = { 1, 1, 1 };
-		testContainer["where"] = { 1, 1, 1, 1 };
-		testContainer["why"] = { 1, 1, 1, 1, 1 };
-		testContainer["how"] = { 1, 1, 1, 1, 1, 1 };
+		testContainer.emplace("the", std::vector<int>(3, 1));
+		testContainer.emplace("and", std::vector<int>(1, 1));
+		testContainer.emplace("who", std::vector<int>(2, 1));
+		testContainer.emplace("what", std::vector<int>(1, 1));
+		testContainer.emplace("when", std::vector<int>(2, 1));
+		testContainer.emplace("where", std::vector<int>(3, 1));
+		testContainer.emplace("why", std::vector<int>(4, 1));
+		testContainer.emplace("how", std::vector<int>(5, 1));
+		testContainer.emplace("when", std::vector<int>(6, 1));
+		testContainer.emplace("then", std::vector<int>(4, 1));
 	}
 	FileManagement fileManager;
 	std::list<std::filesystem::path> paths;
@@ -25,7 +27,7 @@ TEST_F(ReduceAndOutputTest, ReduceTempAndOutputFinal) {
 		reducer1.Reduce(sortedPair.first, sortedPair.second); //takes string of key, and vector of integers of iterations
 
 		// test if the final output file was created with correct format
-		std::filesystem::path outputPath = "../CSE-687_OOD-Project/outputfiles/"; // + path.filename().string();
+		std::filesystem::path outputPath = "../CSE-687_OOD-Project/outputfiles/finalOutput.txt";
 		EXPECT_EQ(std::filesystem::exists(outputPath), true) << "Final Output Failed";
 	}
 }
