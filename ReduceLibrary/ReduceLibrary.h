d4a4bfba0b116dfffd048cbc1a62b45962165bf4
cecolson <cecolson1997@gmail.com>
2022-05-10 18:26:30 -0400

    fixed the ReduceLibrary header file to also include the parameters

--- ReduceLibrary/ReduceLibrary.h
+++ ReduceLibrary/ReduceLibrary.h
@@ -13,8 +13,8 @@
 //extern "C" {
 //#endif
 
-__declspec(dllexport) void Reduce(const std::string & key, const std::vector<int>&iterations);
-void ExportData(const std::string& key, const int reducedData);
+__declspec(dllexport) void Reduce(const std::string & key, const std::vector<int>&iterations, const std::string& outputFilePath);
+void ExportData(const std::string& key, const int reducedData, const std::string& outputFilePath);
 
 //#if __cplusplus
 //}
