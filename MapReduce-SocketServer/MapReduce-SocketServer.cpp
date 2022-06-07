78a5615f5e0263aa6ff21d50fd6dfe77b55e3fdf
Ciante McLemore <ciantemclemore@gmail.com>
2022-06-07 07:18:56 -0400

    Phase 4 project init.

---
+++ MapReduce-SocketServer/MapReduce-SocketServer.cpp
@@ -0,0 +1,12 @@
+// MapReduce-SocketServer.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <WinSock2.h>
+#include <thread>
+
+int main()
+{
+
+    std::cout << "Hello World!\n";
+}
